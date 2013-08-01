b5bcf6a6d3e0e6d3ee452d168e857ea3a680e4f5
Robert McIntosh <rob@robertmcintosh.ca>
2013-08-01 11:24:17 -0400

    Added sendmulti to allow multiple bytes to be written consecutively from javascript Added uart detection so javascript can get a listing of ports

--- SerialAPI.h
+++ SerialAPI.h
@@ -40,16 +40,60 @@ public:
     bool set_option(unsigned int baud, unsigned int parity = 0,
         unsigned int csize = 8, unsigned int flow = 0, unsigned int stop = 0);
 
+	void sendmulti(const FB::JSObjectPtr& msg, int length)
+	{
+		unsigned char *message = new unsigned char[length];
+		FB::variant v;
+		for(int i = 0; i < length; i++)
+		{
+			v = msg->GetProperty(i);
+			message[i] = v.convert_cast<unsigned char>();
+		}
+		io.post(boost::bind(&SerialAPI::do_multi_send, this, (char *)message, length));
+	}
+
     // Send a byte to serial port
-    void send(const char msg)
+    void send(const int msg)
     {
-        io.post(boost::bind(&SerialAPI::do_send, this, msg)); 
+        io.post(boost::bind(&SerialAPI::do_send, this, (const char)msg)); 
     }
+
     bool sendtest()
     {
         io.post(boost::bind(&SerialAPI::do_send, this, 'a'));
         return true;
     }
+
+	void DetectComPorts(std::vector<std::wstring>& list)
+	{
+		for(int i=1; i<=255; i++)	
+		{
+			wchar_t buff[100];
+			wsprintf(buff, TEXT("COM%d"), i);
+			std::wstring wstrPort = buff;
+			//strPort.Format("COM%d",i);
+	
+			DWORD dwSize = 0;
+			LPCOMMCONFIG lpCC = (LPCOMMCONFIG) new BYTE[1];
+			BOOL ret = GetDefaultCommConfig(wstrPort.c_str(), lpCC, &dwSize);
+			delete [] lpCC;	
+
+			lpCC = (LPCOMMCONFIG) new BYTE[dwSize];
+			ret = GetDefaultCommConfig(wstrPort.c_str(), lpCC, &dwSize);
+		
+			if(ret)
+				list.push_back(wstrPort);
+		
+			delete [] lpCC;
+		}
+	}
+
+	FB::VariantList getports() {
+		std::vector<std::wstring> valVec;
+		DetectComPorts(valVec);
+		return FB::make_variant_list(valVec);
+	}
+
     bool is_open()
     {
         return serial.is_open();
@@ -85,9 +129,13 @@ private:
 
     void recv_start(void);
     void recv_complete(const boost::system::error_code& error, size_t bytes_transferred);
-
+	void do_multi_send(const char msg[], int length);
     void do_send(const char msg);
+
+	void send_multi_start(int length);
     void send_start(void);
+
+	void send_multi_complete(const boost::system::error_code& error);
     void send_complete(const boost::system::error_code& error);
 
     void do_close(const boost::system::error_code& error);
