5aa17785b0892e33f6a611f23df15bef15704d8c
Karl Semich <gmkarl@gmail.com>
2015-10-19 07:50:02 -0400

    Add line comment

--- SerialAPI.h
+++ SerialAPI.h
@@ -102,6 +102,7 @@ public:
 
     void close()
     {
+        // check if io thread is not running yet
         if(m_thread.get_id()==boost::thread::id())
         {
             do_close(boost::system::error_code());
